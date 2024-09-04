e26eecaf8b4552781ab1b22a93775379bbcc09f7
Fa-BRAIK <faroukbraik17@gmail.com>
2024-09-04 13:10:54 -0400

    Fix / migration table prefix fixes

--- database/migrations/create_nuxtify_pages_tables.php
+++ database/migrations/create_nuxtify_pages_tables.php
@@ -10,16 +10,23 @@ use Nuxtifyts\NuxtifyPages\Enums\PageVisibility;
 
 return new class extends Migration
 {
+    private ?string $prefix = null;
+
+    private function getPrefix(): string
+    {
+        return $this->prefix ??= (
+        ($prefix = config('nuxtify-pages.database.prefix', ''))
+            ? $prefix . '_'
+            : ''
+        );
+    }
+
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        $prefix = config('nuxtify-pages.database.prefix', '');
-
-        $prefix = $prefix ? $prefix . '_' : '';
-
-        Schema::create("{$prefix}layouts", function (Blueprint $table) {
+        Schema::create("{$this->getPrefix()}layouts", function (Blueprint $table) {
             $table->id();
             $table->string('code', 50)->unique();
             $table->json('metadata')->nullable();
@@ -27,7 +34,7 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        Schema::create("{$prefix}pages", function (Blueprint $table) {
+        Schema::create("{$this->getPrefix()}pages", function (Blueprint $table) {
             $table->id();
             $table->json('slug');
             $table->json('title');
@@ -40,18 +47,23 @@ return new class extends Migration
             $table->timestamp('published_at')->nullable();
             $table->timestamps();
 
-            $table->foreign('layout_id')->references('id')->on('layouts')->nullOnDelete();
+            $table->foreign('layout_id')
+                ->references('id')
+                ->on("{$this->getPrefix()}layouts")
+                ->nullOnDelete();
         });
 
-        Schema::create("{$prefix}tags", function (Blueprint $table) {
+        Schema::create("{$this->getPrefix()}tags", function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
             $table->timestamps();
         });
 
-        Schema::create("{$prefix}taggables", function (Blueprint $table) {
+        Schema::create("{$this->getPrefix()}taggables", function (Blueprint $table) {
             $table->id();
-            $table->foreignId('tag_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('tag_id')
+                ->constrained(table: "{$this->getPrefix()}tags")
+                ->cascadeOnDelete();
             $table->unsignedBigInteger('taggable_id');
             $table->enum('taggable_type', [
                 addslashes(Page::class),
@@ -59,7 +71,7 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        Schema::create("{$prefix}categories", function (Blueprint $table) {
+        Schema::create("{$this->getPrefix()}categories", function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('parent_id')->nullable();
             $table->json('metadata')->nullable();
@@ -67,24 +79,35 @@ return new class extends Migration
             $table->json('description')->nullable();
             $table->timestamps();
 
-            $table->foreign('parent_id')->references('id')->on('categories')->cascadeOnDelete();
+            $table->foreign('parent_id')
+                ->references('id')
+                ->on(table: "{$this->getPrefix()}categories")
+                ->cascadeOnDelete();
         });
 
-        Schema::create("{$prefix}related_categories", function (Blueprint $table) {
+        Schema::create("{$this->getPrefix()}related_categories", function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('related_category_id')->constrained('categories')->cascadeOnDelete();
+            $table->foreignId('category_id')
+                ->constrained(table: "{$this->getPrefix()}categories")
+                ->cascadeOnDelete();
+            $table->foreignId('related_category_id')
+                ->constrained(table: "{$this->getPrefix()}categories")
+                ->cascadeOnDelete();
             $table->enum('relation', array_column(CategoryRelation::cases(), 'value'))->default(CategoryRelation::RELATED);
 
-            $table->unique(['category_id', 'related_category_id']);
+            $table->unique(['category_id', 'related_category_id'], 'category_id_related_category_id_unique');
         });
 
-        Schema::create("{$prefix}category_pages", function (Blueprint $table) {
+        Schema::create("{$this->getPrefix()}category_pages", function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('page_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')
+                ->constrained(table: "{$this->getPrefix()}categories")
+                ->cascadeOnDelete();
+            $table->foreignId('page_id')
+                ->constrained(table: "{$this->getPrefix()}pages")
+                ->cascadeOnDelete();
 
-            $table->unique(['category_id', 'page_id']);
+            $table->unique(['category_id', 'page_id'], 'category_id_page_id_unique');
         });
     }
 
@@ -93,14 +116,14 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('related_categories');
-        Schema::dropIfExists('categories');
-        Schema::dropIfExists('taggables');
-        Schema::dropIfExists('tags');
-        Schema::dropIfExists('components');
-        Schema::dropIfExists('images');
-        Schema::dropIfExists('paragraphs');
-        Schema::dropIfExists('pages');
-        Schema::dropIfExists('layouts');
+        Schema::dropIfExists($this->getPrefix() . 'related_categories');
+        Schema::dropIfExists($this->getPrefix() . 'categories');
+        Schema::dropIfExists($this->getPrefix() . 'taggables');
+        Schema::dropIfExists($this->getPrefix() . 'tags');
+        Schema::dropIfExists($this->getPrefix() . 'components');
+        Schema::dropIfExists($this->getPrefix() . 'images');
+        Schema::dropIfExists($this->getPrefix() . 'paragraphs');
+        Schema::dropIfExists($this->getPrefix() . 'pages');
+        Schema::dropIfExists($this->getPrefix() . 'layouts');
     }
 };
